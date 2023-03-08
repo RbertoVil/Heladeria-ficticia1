8019cd111fa9b95d90fbcef469c2104a466bacac
Rberto Vil <rantoniovillasana@gmail.com>
2023-03-08 02:42:48 -0400

    Update app.py to add the pages

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ import databaseConfig
 
 app = Flask(__name__)
 
-#cursor.execute("SELECT * FROM menu;")
-#for i in cursor:
-#    print(i)
-
 @app.route("/")
 def heladeria():
     return render_template("index.html")
@@ -25,3 +21,27 @@ def menu():
     conexion.close()
 
     return render_template("menu.html", lineas = lineas)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    error = None
+    return render_template("login.html", error = error)
+
+@app.route("/admin", methods=["GET", "POST"])
+def admin():
+    # Si intentan ingresar a /admin de forma incorrecta
+    if request.method == "POST":
+        if request.form["username"] != "admin" or request.form["password"] != "r1234":
+            # credenciales incorrectas
+            error = " Credenciales incorrectas."
+            return render_template("login.html", error = error)
+    elif request.method == "GET":
+        # Si intentan ingresar a /admin de forma directa
+        return render_template("login.html")
+
+# Para eliminar y editar datos usar el "i"
+#cursor.execute("SELECT * FROM menu;")
+#for i in cursor:
+#    print(i)
+
+    return render_template("admin.html")
